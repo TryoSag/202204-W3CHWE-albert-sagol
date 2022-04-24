1b36919bc34958be9a817aeda8b79793490b8732
Albert Sagol <albert.sagol@gmail.com>
2022-04-24 09:17:27 +0200

    added master component and pokemon extend

---
+++ web/js/components/PokemonComponent/PokemonComponent.js
@@ -0,0 +1,39 @@
+import Component from "../Component.js";
+
+class PokemonComponent extends Component {
+  constructor(parentElement, pokemon) {
+    pokemon;
+    super(parentElement, "li", "");
+    this.pokemon = pokemon;
+
+    render();
+    addListeners();
+  }
+  render = () => {
+    this.element.innerHTML = `
+      <div>
+        <img src="${this.pokemon.image}" alt="${this.pokemon.name} picture" />
+        <p>${this.pokemon.name}</p>
+      </div>
+      <div>
+        <button class="button--details pokemon-details-${this.pokemon.id}">More details</button>
+        <i class="icon--pokeball catch-pokemon-${this.pokemon.id}"
+          ><img src="img/pokeball.png" alt="pokeball image"
+        /></i>
+      </div>
+    `;
+  };
+  addListeners = () => {
+    const detailsButton = document.querySelector(
+      `pokemon-details-${this.pokemon.id}`
+    );
+    detailsButton.addEventListener("click");
+
+    const catchedPokemon = document.querySelector(
+      `catch-pokemon-${this.pokemon.id}`
+    );
+    catchedPokemon.addEventListener("click");
+  };
+}
+
+export default PokemonComponent;
